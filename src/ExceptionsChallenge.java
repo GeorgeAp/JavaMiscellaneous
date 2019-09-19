1a132a9b2f78b6a74c5350efe6b429122f8b6a7d
georges <george.apaaboah@gmail.com>
2019-09-19 11:16:52 +0200

    new file commit

---
+++ src/ExceptionsChallenge.java
@@ -0,0 +1,42 @@
+import java.io.Closeable;
+import java.io.IOException;
+
+public class ExceptionsChallenge {
+
+    static String marvelHero = "";
+
+    public static void main(String... marvel) throws Exception{
+        Logan logan = new Logan();
+        new ExceptionsChallenge().executeAction(new PeterParker(), logan);
+        System.out.println(marvelHero + logan.wolverineCloseCount);
+    }
+
+    private void executeAction(Closeable spiderMan, AutoCloseable wolverine) throws Exception{
+        try(spiderMan; wolverine){
+            wolverine.close();
+        }
+        catch (Exception ignore){
+            marvelHero += "?";
+            spiderMan.close();
+        }
+    }
+
+    static class PeterParker implements Closeable{
+        public void close(){
+            marvelHero += "#";
+        }
+    }
+
+    static class Logan implements AutoCloseable{
+        int wolverineCloseCount = 0;
+
+        public void close() throws IOException{
+            marvelHero += ">";
+            wolverineCloseCount++;
+
+            if (wolverineCloseCount >= 1){
+                throw new IOException();
+            }
+        }
+    }
+}
