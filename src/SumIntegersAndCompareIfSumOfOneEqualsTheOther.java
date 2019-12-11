38a973f45222f114016fc21672f14676f65ff14d
georgeap <george.apaaboah@gmail.com>
2019-12-11 11:03:35 +0100

    Updated file commit

--- src/SumIntegersAndCompareIfSumOfOneEqualsTheOther.java
+++ src/SumIntegersAndCompareIfSumOfOneEqualsTheOther.java
@@ -11,6 +11,8 @@ public class SumIntegersAndCompareIfSumOfOneEqualsTheOther {
 
         int a = 0;
         int b = 0;
+
+        // check if input is an integer
         if(sc.hasNextInt()){
             a = sc.nextInt();
             b = sc.nextInt();
@@ -20,23 +22,26 @@ public class SumIntegersAndCompareIfSumOfOneEqualsTheOther {
             return;
         }
 
-
+        // Call the IntegerSumCheck function to perform the service
         boolean result = IntegerSumCheck(a, b);
 
         System.out.println("The result is: " + result);
     }
 
+    // Here, we convert the given integer 'a' into a string the take each character in 'a',
+    // convert it back to integer then sum.
     public static boolean IntegerSumCheck(int a, int b){
         // We assume b > a
         String new_a = Integer.toString(a);
         int sum_value = 0; // sums the digits in a number eg. 23 => 2+3 = 5
         for(int i = 0; i < new_a.length(); i++){
-            int j = Integer.parseInt(String.valueOf(new_a.charAt(i)));
-            sum_value += j;
+            int digit_at_i = Integer.parseInt(String.valueOf(new_a.charAt(i))); // extract the digit at i
+            sum_value += digit_at_i;
         }
         int new_sum_value = a + sum_value;
 
-        if(b==new_sum_value){
+        if(b == new_sum_value){  // compares the sum of the digits in input 'a' + 'a' to b
+                                 // returns true if validated else false
             return true;
         }
 
