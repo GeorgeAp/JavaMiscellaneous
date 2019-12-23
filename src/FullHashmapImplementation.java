9656e7b5f1c341a05255b314d0ffc1f7d22637c9
georgeap <george.apaaboah@gmail.com>
2019-12-23 23:05:15 +0100

    Updated files commit - includes Test file

--- src/FullHashmapImplementation.java
+++ src/FullHashmapImplementation.java
@@ -2,10 +2,111 @@ import java.util.Objects;
 
 public class FullHashmapImplementation<K, V> {
 
+    // Insering elements
+        private Entry<K, V>[] buckets;
+        private int capacity;
+        private int size = 0;
+
+        private double lf = 0.75;
+
+
+        public FullHashmapImplementation(){
+            this(16);
+        }
+
+        public FullHashmapImplementation(int capacity) {
+            this.capacity = capacity;
+            this.buckets = new Entry[this.capacity];
+        }
+
+        public void put(K key, V value){
+            if (size == lf * capacity){
+                // rehash
+                Entry<K, V>[] old = buckets;
+
+                capacity *= 2;
+                size = 0;
+                buckets = new Entry[capacity];
+
+                for (Entry<K, V> e : old){
+                    while (e != null){
+                        put(e.key, e.value);
+                        e = e.next;
+                    }
+                }
+            }
+
+            Entry<K, V> entry = new Entry<>(key, value, null);
+            int bucket = getHash(key) % getBucketSize();
+
+            Entry<K, V> existing = buckets[bucket];
+            if (existing == null){
+                buckets[bucket] = entry;
+                size++;
+            }
+            else {
+                // compare the keys to see if they already exist
+                while (existing.next != null) {
+                    if (existing.key.equals(key)) {
+                        existing.value = value;
+                        return;
+                    }
+                    existing = existing.next;
+                }
+                if (existing.key.equals(key)){
+                    existing.value = value;
+                }
+                else {
+                    existing.next = entry;
+                    size++;
+                }
+            }
+        }
+
+        public V get(K key) {
+            Entry<K, V> bucket = buckets[getHash(key) % getBucketSize()];
+
+            while (bucket != null) {
+                if (key == bucket.key) {
+                    return bucket.value;
+                }
+                bucket = bucket.next;
+            }
+            return null;
+        }
+
+        public int size() {
+            return size;
+        }
+
+        private int getBucketSize() {
+            return buckets.length;
+        }
+
+        private int getHash(K key) {
+            return key == null ? 0 : Math.abs(key.hashCode());
+        }
+
+        @Override
+        public String toString(){
+            StringBuilder stringBuilder = new StringBuilder();
+            for (Entry entry: buckets){
+                stringBuilder.append("[");
+                while (entry != null){
+                    stringBuilder.append(entry);
+                    if (entry.next != null)
+                        stringBuilder.append(",");
+                    entry = entry.next;
+                }
+                stringBuilder.append("]");
+            }
+            return "{" + stringBuilder.toString() + "}";
+        }
+
+
     // Hashmap model
 
-    class Entry<K, V>{
-
+    static class Entry<K, V>{
         final K key;
         V value;
         Entry<K, V> next;
@@ -50,74 +151,11 @@ public class FullHashmapImplementation<K, V> {
         public int hashCode() {
             return Objects.hash(key, value, next);
         }
-    }
 
-    // Insering elements
-    public class MyMap<K, V>{
-        private Entry<K, V>[] buckets;
-        private static final int INITIAL_CAPACITY = 1 << 4;
-
-        private int size = 0;
-
-        public MyMap(){
-            this(INITIAL_CAPACITY);
+        @Override
+        public String toString() {
+            return "{" + key + ", " + value + "}";
         }
-
-        public MyMap(int capacity) {
-            this.buckets = new Entry[capacity];
-        }
-
-        public void put(K key, V value){
-            Entry<K, V> entry = new Entry<>(key, value, null);
-            int bucket = getHash(key) % getBucketSize();
-
-            Entry<K, V> existing = buckets[bucket];
-            if (existing == null){
-                buckets[bucket] = entry;
-                size++;
-            }
-            else {
-                // Compare the keys to check if they they already exist
-                while (existing.next != null){
-                    if (existing.key.equals(key)){
-                        existing.value = value;
-                        return;
-                    }
-                    existing = existing.next;
-                }
-                if (existing.key.equals(key))
-                    existing.value = value;
-                else {
-                    existing.next = entry;
-                    size++;
-                }
-
-            }
-        }
-
-        public V get(K key) {
-            Entry<K, V> bucket = buckets[getHash(key) % getBucketSize()];
-
-            while (bucket != null) {
-                if (key == bucket.key) {
-                    return bucket.value;
-                }
-                bucket = bucket.next;
-            }
-            return null;
-        }
-
-        public int size() {
-            return size;
-        }
-
-        private int getBucketSize() {
-            return buckets.length;
-        }
-
-        private int getHash(K key) {
-            return key == null ? 0 : Math.abs(key.hashCode());
-        }
-
     }
 }
+
--- src/FullHashmapImplementationTest.java
+++ src/FullHashmapImplementationTest.java
@@ -1,8 +1,21 @@
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 public class FullHashmapImplementationTest {
     @Test
     public void testHashmap(){
+
         FullHashmapImplementation<String, String> fullHashmapImplementation = new FullHashmapImplementation<>();
+        fullHashmapImplementation.put("France", "Paris");
+        fullHashmapImplementation.put("Italy", "Rome");
+        fullHashmapImplementation.put("Germany", "Berlin");
+        fullHashmapImplementation.put("Belgium", "Brussels");
+
+        assertNotNull("The object is not empty",fullHashmapImplementation);
+        assertEquals(4, fullHashmapImplementation.size());
+        assertEquals("Berlin", fullHashmapImplementation.get("Germany"));
+        assertEquals("Paris", fullHashmapImplementation.get("France"));
     }
 }
