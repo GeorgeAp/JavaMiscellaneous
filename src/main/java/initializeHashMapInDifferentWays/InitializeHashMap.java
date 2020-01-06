dd2719f24c15a4a61f1f46324aa4a631eee90461
georgeap <george.apaaboah@gmail.com>
2020-01-06 18:06:30 +0100

    File update commit

--- src/main/java/initializeHashMapInDifferentWays/InitializeHashMap.java
+++ src/main/java/initializeHashMapInDifferentWays/InitializeHashMap.java
@@ -2,6 +2,8 @@ package initializeHashMapInDifferentWays;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class InitializeHashMap {
 
@@ -17,5 +19,29 @@ public class InitializeHashMap {
     but it will work only for static. Entries could be added and removed as and when required.
      */
     }
+
+    // Initialize a map using Java 8 Stream API
+    public static Map<String,String> streamMapInitialize(){
+        Map<String, String> map1 = Stream.of(new String[][]{
+                {"Hello", "World"},
+                {"John", "Doe"},
+        }).collect(Collectors.toMap(data -> data[0], data -> data[1]));
+        return map1;
+    }
+    // In order to make it more generic let's take the array of Objects and perform the same operation:
+    public static Map<String, Integer> streamMapInitializeWithObject(){
+        Map<String, Integer> map2 = Stream.of(new Object[][]{
+                {"Hello", 1},
+                {"John", 2},
+        }).collect(Collectors.toMap(data ->(String) data[0], data -> (Integer) data[1]));
+        return map2;
+    }
+
+    public static void main(String[] args) {
+        streamMapInitialize().put("Kofi", "Ama");
+        streamMapInitializeWithObject().put("Yaw", 1);
+    }
+
+
 
 }
