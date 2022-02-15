68f2b74bcdbe4ff435925b21cb59d1222d93c7f7
George Apaaboah <george.apaaboah@gmail.com>
2022-02-15 19:05:43 +0100

    Update license

--- src/main/java/simpleSortAlogrithms/ASimpleSortAlgo.java
+++ src/main/java/simpleSortAlogrithms/ASimpleSortAlgo.java
@@ -1,4 +1,4 @@
-/* SPDX-License-Identifier: Apache-2.0 */
+// SPDX-License-Identifier: Apache-2.0
 package simpleSortAlogrithms;
 
 /**
@@ -9,7 +9,9 @@ package simpleSortAlogrithms;
 public class ASimpleSortAlgo {
     public static void main(String[] args) {
         // Example array
-        int[] valuesToBeSorted = new int[]{6, 3, 4, 0, 1,3,4, 90, 1, 0, 2, 32};
+        int[] valuesToBeSorted = 
+        new int[]{6, 3, 4, 0, 1,3,4, 90, 1, 0, 2, 32};
+        
         for (int i = 0; i < valuesToBeSorted.length; i++) {
             int temp = valuesToBeSorted[i];
             for (int j = 0; j < valuesToBeSorted.length; j++) {
