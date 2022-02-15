720bf0f5338afa125f8bf308f7225cf2158a2625
George Apaaboah <george.apaaboah@gmail.com>
2022-02-15 11:00:58 +0100

    test github action

--- src/main/java/simpleSortAlogrithms/ASimpleSortAlgo.java
+++ src/main/java/simpleSortAlogrithms/ASimpleSortAlgo.java
@@ -9,7 +9,9 @@ package simpleSortAlogrithms;
 public class ASimpleSortAlgo {
     public static void main(String[] args) {
         // Example array
-        int[] valuesToBeSorted = new int[]{6, 3, 4, 0, 1,3,4, 90, 1, 0, 2, 32};
+        int[] valuesToBeSorted = 
+        new int[]{6, 3, 4, 0, 1,3,4, 90, 1, 0, 2, 32};
+        
         for (int i = 0; i < valuesToBeSorted.length; i++) {
             int temp = valuesToBeSorted[i];
             for (int j = 0; j < valuesToBeSorted.length; j++) {
